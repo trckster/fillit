37f21263dcf76d2610971652b2c878c5e717fca0
Daniil <dan-trofimov@mail.ru>
2019-10-12 15:37:40 +0300

    change privs

--- fillit/main.c
+++ fillit/main.c
@@ -6,12 +6,22 @@
 /*   By: bkayleen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/12 14:16:14 by bkayleen          #+#    #+#             */
-/*   Updated: 2019/10/12 14:34:42 by bkayleen         ###   ########.fr       */
+/*   Updated: 2019/10/12 14:50:39 by bkayleen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
+void	show_usage()
+{
+	ft_putstr("usage: <>< ><> < <<<<< >>>>");
+}
+
 int		main(int argc, char **argv)
 {
+	if (argc != 2)
+	{
+		show_usage();
+		return ;
+	}
 }
