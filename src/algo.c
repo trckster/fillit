ef3e6ebc9d8997259a44bb2dbae801afb4014936
Daniil <dan-trofimov@mail.ru>
2019-10-13 16:39:03 +0300

    add main logic

--- src/algo.c
+++ src/algo.c
@@ -6,7 +6,7 @@
 /*   By: bkayleen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/13 16:00:58 by bkayleen          #+#    #+#             */
-/*   Updated: 2019/10/13 16:15:40 by bkayleen         ###   ########.fr       */
+/*   Updated: 2019/10/13 16:38:44 by bkayleen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,41 @@ int		get_init_square_size(t_item *items)
 	return (ft_sqrt(size * 4));
 }
 
+void	clear_square(char *square, int size)
+{
+	int i;
+
+	i = 0;
+	while (i < size * size)
+		square[i++] = '.';
+}
+
+void	fill_square(char *sq, t_item *items, int curr_item)
+{
+	if (!items[curr_item])
+		return (1);
+}
+
+int		try_accommodate(t_item *items, int square_size)
+{
+	char	*square;
+
+	square = (char *)malloc(square_size * square_size);
+	clear_square(square, square_size);
+	if (fill_square(square, items, 0))
+	{
+		print_answer(square);
+		return (1);
+	}
+	return (0);
+}
+
 int		process_algorithm(t_item *items)
 {
-	int		init_size;
+	int		square_size;
 
-	init_size = get_init_square_size(items);
+	square_size = get_init_square_size(items);
+	while (!try_accommodate(items, square_size))
+		square_size++;
+	return (1);
 }
