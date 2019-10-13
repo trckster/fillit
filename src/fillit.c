dffe16e61aeb84afa5c999671c67a31a0fb233d6
Daniil <dan-trofimov@mail.ru>
2019-10-13 15:55:36 +0300

    fillit.h

--- src/fillit.c
+++ src/fillit.c
@@ -6,7 +6,7 @@
 /*   By: bkayleen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/12 14:16:14 by bkayleen          #+#    #+#             */
-/*   Updated: 2019/10/12 14:50:39 by bkayleen         ###   ########.fr       */
+/*   Updated: 2019/10/13 15:55:14 by bkayleen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@ int     fillit(char *filename)
 {
 	if (!filename)
 		return (0);
+	t_item *data[26];
 //	check_file_input(filename);
-//  load_data_from_file();
-//  process_(recursive?)_algorithm()
+//  load_data_from_file(data);
+//  process_(recursive?)_algorithm(data)
 //  print_answer()
 //  return 0 or 1;
 	return (1);
--- src/fillit.h
+++ src/fillit.h
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkayleen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/13 15:57:29 by bkayleen          #+#    #+#             */
+/*   Updated: 2019/10/13 15:57:36 by bkayleen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
@@ -6,7 +17,11 @@
 # include "../libft/libft.h"
 # include <fcntl.h>
 
+typedef struct	s_item
+{
+	char field[4][4];
+}				t_item;
+
 int         fillit(char *s);
-//int		valid_tetramino(const char *shape, t_point ***points);
 
 #endif
